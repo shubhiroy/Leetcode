bfd434eadfb2708736cdaf583fef6ac0f64b2386
Shubham Roy <shubham.roy@in.abb.com>
2023-12-16 18:43:25 +0000

    Sync LeetCode submission - Next Permutation (cpp)

---
+++ problems/next_permutation/solution.cpp
@@ -0,0 +1,51 @@
+#include <algorithm>
+#include <iostream>
+
+// TC - O(3N), SC-O(1)
+class Solution
+{
+public:
+    void nextPermutation(vector<int> &nums)
+    {
+        if (nums.size() == 0 || nums.size() == 1)
+        {
+            return;
+        }
+
+        int i = nums.size() - 2;
+        for (; i >= 0; i--)
+        {
+            if (nums[i] < nums[i + 1])
+            {
+                break;
+            }
+        }
+
+        if (i > -1)
+        {
+            int j = nums.size() - 1;
+            while (j != i && nums[j] <= nums[i])
+            {
+                j--;
+            }
+            std::swap(nums[i], nums[j]);
+            // int temp = nums[i];
+            // nums[i] = nums[j];
+            // nums[j] = temp;
+        }
+        std::reverse(nums.begin() + i + 1, nums.end());
+    }
+};
+
+// STL function
+// class Solution
+// {
+// public:
+//     void nextPermutation(vector<int> &nums)
+//     {
+//         std::next_permutation(nums.begin(), nums.end());
+//     }
+// };
+
+// Brute force, Creating all permutation of the array
+// TC - N!*N, SC -
