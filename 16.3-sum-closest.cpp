e2da902c2f899f9eb83295649b4d8f2ddcee3c01
Shubham Roy <shubham.roy@in.abb.com>
2023-12-19 01:12:53 +0530

    3Sum closest

--- 16.3-sum-closest.cpp
+++ 16.3-sum-closest.cpp
@@ -5,11 +5,36 @@
  */
 
 // @lc code=start
-class Solution {
+#include <iostream>
+#include <climits>
+class Solution
+{
 public:
-    int threeSumClosest(vector<int>& nums, int target) {
-        
+    int threeSumClosest(vector<int> &nums, int target)
+    {
+        int closestSum = INT_MAX;
+        std::sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int j = i + 1, k = nums.size() - 1;
+            while (j < k)
+            {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (std::abs((long)target - sum) < std::abs((long)target - closestSum))
+                {
+                    closestSum = sum;
+                }
+                if (sum > target)
+                {
+                    k--;
+                }
+                else
+                {
+                    j++;
+                }
+            }
+        }
+        return closestSum;
     }
 };
 // @lc code=end
-
