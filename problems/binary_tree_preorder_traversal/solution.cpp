083d62dc8083bc8adc0fb0ed9c88e0cb68c00350
Shubham Roy <shubham.roy@in.abb.com>
2024-02-24 14:06:11 +0000

    Sync LeetCode submission - Binary Tree Preorder Traversal (cpp)

--- problems/binary_tree_preorder_traversal/solution.cpp
+++ problems/binary_tree_preorder_traversal/solution.cpp
@@ -9,27 +9,61 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+// #include <vector>
+// class Solution
+// {
+// public:
+//     vector<int> preorderTraversal(TreeNode *root)
+//     {
+//         std::vector<int> preorder;
+//         if (root == NULL)
+//         {
+//             return preorder;
+//         }
+//         stack<TreeNode *> s;
+//         s.push(root);
+//         while (!s.empty())
+//         {
+//             TreeNode *temp = s.top();
+//             s.pop();
+//             preorder.push_back(temp->val);
+//             if (temp->right != NULL)
+//             {
+//                 s.push(temp->right);
+//             }
+//             if (temp->left != NULL)
+//             {
+//                 s.push(temp->left);
+//             }
+//         }
+//         return preorder;
+//     }
+// };
+
+#include <vector>
+class Solution
+{
 public:
-    vector<int> preorderTraversal(TreeNode* root) {
+    vector<int> preorderTraversal(TreeNode *root)
+    {
         vector<int> preorder;
-        if(root == NULL){
+        if (root == NULL)
+        {
             return preorder;
         }
-        stack<TreeNode*> s;
-        s.push(root);
-        while(!s.empty()){
-            TreeNode* temp = s.top();
-            s.pop();
-            preorder.push_back(temp->val);
-            if(temp->right != NULL){
-                s.push(temp->right);
-            }
-            if(temp->left != NULL){
-                s.push(temp->left);
-            }
-        }
+        preOrder(root, preorder);
         return preorder;
-        
+    }
+
+    void preOrder(TreeNode *root, vector<int> &preorder)
+    {
+        if (root == NULL)
+        {
+            return;
+        }
+
+        preorder.push_back(root->val);
+        preOrder(root->left, preorder);
+        preOrder(root->right, preorder);
     }
 };
