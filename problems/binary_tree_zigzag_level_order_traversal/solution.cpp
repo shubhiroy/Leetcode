9339983db16c60888ef89c141bc4b5ea4b08cf7e
Shubham Roy <shubham.roy@in.abb.com>
2024-02-29 14:53:17 +0000

    Sync LeetCode submission - Binary Tree Zigzag Level Order Traversal (cpp)

--- problems/binary_tree_zigzag_level_order_traversal/solution.cpp
+++ problems/binary_tree_zigzag_level_order_traversal/solution.cpp
@@ -9,65 +9,96 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution
-{
+// class Solution
+// {
+// public:
+//     vector<vector<int>> zigzagLevelOrder(TreeNode *root)
+//     {
+//         vector<vector<int>> zigzag;
+//         if (root == NULL)
+//             return zigzag;
+//         deque<TreeNode *> dq;
+//         dq.push_back(root);
+//         dq.push_back(NULL);
+//         TreeNode *node = root;
+//         int reverse = 1;
+//         vector<int> lvl;
+//         while (!dq.empty())
+//         {
+//             TreeNode *temp;
+//             // cout<<reverse;
+//             if (reverse == 1)
+//             {
+//                 temp = dq.front();
+//                 dq.pop_front();
+//             }
+//             else
+//             {
+//                 temp = dq.back();
+//                 dq.pop_back();
+//             }
+//             if (temp == NULL)
+//             {
+//                 reverse = reverse * -1;
+//                 if (!dq.empty())
+//                 {
+//                     if (reverse == 1)
+//                         dq.push_back(NULL);
+//                     else
+//                         dq.push_front(NULL);
+//                 }
+//                 zigzag.push_back(lvl);
+//                 lvl = {};
+//             }
+//             else
+//             {
+//                 lvl.push_back(temp->val);
+//                 if (reverse == 1)
+//                 {
+//                     if (temp->left != NULL)
+//                         dq.push_back(temp->left);
+//                     if (temp->right != NULL)
+//                         dq.push_back(temp->right);
+//                 }
+//                 else
+//                 {
+//                     if (temp->right != NULL)
+//                         dq.push_front(temp->right);
+//                     if (temp->left != NULL)
+//                         dq.push_front(temp->left);
+//                 }
+//             }
+//         }
+//         return zigzag;
+//     }
+// };
+
+
+class Solution {
 public:
-    vector<vector<int>> zigzagLevelOrder(TreeNode *root)
-    {
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         vector<vector<int>> zigzag;
-        if (root == NULL)
-            return zigzag;
-        deque<TreeNode *> dq;
-        dq.push_back(root);
-        dq.push_back(NULL);
-        TreeNode *node = root;
-        int reverse = 1;
-        vector<int> lvl;
-        while (!dq.empty())
-        {
-            TreeNode *temp;
-            // cout<<reverse;
-            if (reverse == 1)
-            {
-                temp = dq.front();
-                dq.pop_front();
-            }
-            else
-            {
-                temp = dq.back();
-                dq.pop_back();
-            }
-            if (temp == NULL)
-            {
-                reverse = reverse * -1;
-                if (!dq.empty())
-                {
-                    if (reverse == 1)
-                        dq.push_back(NULL);
-                    else
-                        dq.push_front(NULL);
+        if(root==NULL) return zigzag;
+        queue<TreeNode*> q;
+        bool reverse = false;
+        q.push(root);
+        while(!q.empty()){
+            int size = q.size();
+            vector<int> row(size);
+            for(int i=0; i<size; i++){
+                TreeNode* node = q.front();
+                q.pop();
+                int index = !reverse ? i : size-i-1;
+                row[index] = node->val;
+                if(node->left){
+                    q.push(node->left);
                 }
-                zigzag.push_back(lvl);
-                lvl = {};
-            }
-            else
-            {
-                lvl.push_back(temp->val);
-                if (reverse == 1)
-                {
-                    if (temp->left != NULL)
-                        dq.push_back(temp->left);
-                    if (temp->right != NULL)
-                        dq.push_back(temp->right);
-                }
-                else
-                {
-                    if (temp->right != NULL)
-                        dq.push_front(temp->right);
-                    if (temp->left != NULL)
-                        dq.push_front(temp->left);
+                if(node->right){
+                    q.push(node->right);
                 }
             }
+            reverse = !reverse;
+            zigzag.push_back(row);
         }
         return zigzag;
     }
